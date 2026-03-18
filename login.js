750fbb202be03ff14373007859258a190a022866
justindavis882 <60278370+justindavis882@users.noreply.github.com>
2026-03-18 08:08:43 -0400

    Update login.js

--- login.js
+++ login.js
@@ -2,16 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/fireba
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
-// Initialize Firebase (Paste your exact config here)
-const firebaseConfig = {
-  apiKey: "AIzaSyCLkAIMy7R5UEoirN4CaVWuKJbCxzyQBVI",
-  authDomain: "simplesis-f3606.firebaseapp.com",
-  projectId: "simplesis-f3606",
-  storageBucket: "simplesis-f3606.firebasestorage.app",
-  messagingSenderId: "217211857685",
-  appId: "1:217211857685:web:56bc8f3e196d076599d71c",
-  measurementId: "G-JKEFLYBDQK"
-};
+import { firebaseConfig } from "./config.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);

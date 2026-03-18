e9e0709adfd0d890c4bb57a72531d61eb37433f7
justindavis882 <60278370+justindavis882@users.noreply.github.com>
2026-03-18 08:01:50 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCLkAIMy7R5UEoirN4CaVWuKJbCxzyQBVI",
+  authDomain: "simplesis-f3606.firebaseapp.com",
+  projectId: "simplesis-f3606",
+  storageBucket: "simplesis-f3606.firebasestorage.app",
+  messagingSenderId: "217211857685",
+  appId: "1:217211857685:web:56bc8f3e196d076599d71c"
+};
